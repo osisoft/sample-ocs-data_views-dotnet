fa02f9dfe7a81657baae60929537ecc31767a3b4
Kelsey Duffy <kduffy@osisoft.com>
2021-05-01 14:48:09 -0400

    initial release

---
+++ DataViews/SampleType1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OSIsoft.Data;
+
+namespace DataViews
+{
+    public class SampleType1
+    {
+        #region step2
+        [SdsMember(IsKey = true)]
+        public DateTime Time { get; set; }
+
+        public double Pressure { get; set; }
+        public double Temperature { get; set; }
+        #endregion // step2
+    }
+}
---
+++ DataViewsTests/UnitTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace DataViewsTests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void DataViewsTest()
+        {
+            Assert.True(DataViews.Program.MainAsync(true).Result);
+        }
+    }
+}
